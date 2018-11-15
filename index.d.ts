f7d8daab91e21aac7b99a5a0f1f7b2f1c3f983fd
Joana Costa <joana.costa@chauffeur-prive.com>
2018-11-15 09:06:29 +0100

    Bump package to next minor version

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for stakhanov 0.6.1
+// Type definitions for stakhanov 0.7.0
 // Project: stakhanov
 // Definitions by: Chauffeur Privé
 // TypeScript Version: 3.0.1
@@ -6,49 +6,53 @@
 /// <reference types="node" />
 
 interface Logger {
-    debug: () => void;
-    info: () => void;
-    warn: () => void;
-    error: () => void;
-}
-
-declare module Stakhanov {
-    export function createWorkers(handlers: Stakhanov.Handler[], config: Stakhanov.Config, options?: Stakhanov.Options): Stakhanov.Process
+  debug: () => void;
+  info: () => void;
+  warn: () => void;
+  error: () => void;
 }
 
 declare namespace Stakhanov {
-    export interface Process {
-        listen: () => void;
-        close: (forceExit?: Boolean) => void;
-        wait: (eventName: string, timeout?: number) => Promise<void>;
-        TASK_COMPLETED: string;
-        TASK_RETRIED: string;
-        TASK_FAILED: string;
-        WORKER_CLOSED: string;
-    }
+  export function createWorkers(
+    handlers: Stakhanov.Handler[],
+    config: Stakhanov.Config,
+    options?: Stakhanov.Options
+  ): Stakhanov.Process;
+}
 
-    export interface Handler<TBusMessage = any, TValidatedMessage = any> {
-        routingKey: string;
-        validate: (message: TBusMessage) => TValidatedMessage;
-        handle: (message: TValidatedMessage) => void;
-    }
+declare namespace Stakhanov {
+  export interface Process {
+    listen: () => void;
+    close: (forceExit?: Boolean) => void;
+    wait: (eventName: string, timeout?: number) => Promise<void>;
+    TASK_COMPLETED: string;
+    TASK_RETRIED: string;
+    TASK_FAILED: string;
+    WORKER_CLOSED: string;
+  }
 
-    export interface Config {
-        workerName: string;
-        amqpUrl: string;
-        exchangeName: string;
-        queueName: string;
-    }
+  export interface Handler<TBusMessage = any, TValidatedMessage = any> {
+    routingKey: string;
+    validate: (message: TBusMessage) => TValidatedMessage;
+    handle: (message: TValidatedMessage) => void;
+  }
 
-    export interface Options {
-        heartbeat?: number;
-        taskTimeout?: number;
-        processExitTimeout?: number;
-        channelPrefetch?: number;
-        closeOnSignals?: Boolean;
-        channelCloseTimeout?: number;
-        logger?: Logger;
-    }
+  export interface Config {
+    workerName: string;
+    amqpUrl: string;
+    exchangeName: string;
+    queueName: string;
+  }
+
+  export interface Options {
+    heartbeat?: number;
+    taskTimeout?: number;
+    processExitTimeout?: number;
+    channelPrefetch?: number;
+    closeOnSignals?: Boolean;
+    channelCloseTimeout?: number;
+    logger?: Logger;
+  }
 }
 
 export = Stakhanov;
